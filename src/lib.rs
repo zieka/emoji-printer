d987901748e830ac11cc902897cadd17c00fd4d9
Kyle Scully <scullykns@gmail.com>
2021-12-27 15:24:06 -0800

    feat: add print_emojis (text: &str) -> std::string::String

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+mod emojis;
+
+pub fn print_emojis(text: &str) -> std::string::String {
+    let emojis_map = emojis::get_emojis_hash_map();
+
+    let words = text.split_whitespace();
+
+    words.map(|word| {
+        if emojis_map.contains_key(word){
+            return emojis_map.get(word).unwrap().to_owned().to_string();
+        } 
+        word.to_string()
+    }).collect::<Vec<String>>().join(" ")
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_prints_emojis() {
+        let result = print_emojis("Do you like :sushi: sushi?");
+        assert_eq!(result, "Do you like 🍣 sushi?");
+    }
+
+    #[test]
+    fn it_leaves_unsupported_emojis() {
+        let result = print_emojis("Do you like :not_a_supported_emoji: sushi?");
+        assert_eq!(result, "Do you like :not_a_supported_emoji: sushi?");
+    }
+}
--- src/main.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    println!("Check back later!");
-}
